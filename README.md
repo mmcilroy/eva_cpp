e248349d4373081f6400a9a43dabee99fcc4a7ef
sm87110 <mark.mcilroy@citi.com>
2014-10-07 11:08:54 +0100

    test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 eva_cpp
 =======
-
+test
 A framework for writing event driven applications 
 
     while true; do make; sleep 5; done
--- src/event_queue.cpp
+++ src/event_queue.cpp
@@ -6,7 +6,7 @@
 using namespace eva;
 
 event_queue::event_queue( event_queue_options opt ) :
-    _events( 1024 ),
+    _events( 8*1024 ),
     _publisher( 0 ),
     _subscriber( 0 ),
     _journal_subscriber( 0 ),
